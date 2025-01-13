b4fc9f0ff568a6d7e2f22275c634e5d8fc565e16
Sakura286 <sakura286@outlook.com>
2025-01-13 14:10:40 +0800

    add std::variant

--- chrome/browser/ai/ai_context_bound_object_set.h
+++ chrome/browser/ai/ai_context_bound_object_set.h
@@ -5,6 +5,8 @@
 #ifndef CHROME_BROWSER_AI_AI_CONTEXT_BOUND_OBJECT_SET_H_
 #define CHROME_BROWSER_AI_AI_CONTEXT_BOUND_OBJECT_SET_H_
 
+#include <variant>
+
 #include "base/containers/unique_ptr_adapters.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ai/ai_context_bound_object.h"
